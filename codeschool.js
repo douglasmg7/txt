4689b4698e2802bb92871baffce03a35a89bd87e
Douglas Gomes <douglasmg7@gmail.com>
2016-03-29 11:09:03 -0300

    organization

--- codeschool.js
+++ codeschool.js
@@ -1,35 +1,8 @@
-var vehicle3 = {
-  type: "Submarine", capacity: 8, storedAt: "Underwater Outpost",
-  ranger1: {name: "Gregg Pollack", skillz: "Lasering", dayOff: "Friday"},
-  ranger2: {name: "Bijan Boustani", skillz: "Working", dayOff: "Saturday"},
-  ranger3: {name: "Ashley Smith", skillz: "Torpedoing", dayOff: "Friday"},
-  ranger4: {name: "Mark Krupinski", skillz: "Sniping", dayOff: "Wednesday"},
-  numRangers: 4
-};
+var shoe = {color: 'red'};
+var s2 = Object.create(shoe);
 
-function relieveDuty (vehicle, day) {
-  var offDuty = [];
-  var onDuty = [];
-  for (var i = 0; i < vehicle.numRangers; i++) {
-    if (vehicle['ranger' + (i + 1)].dayOff == day) {
-    	offDuty.push(vehicle['ranger' + (i + 1)]);
-    }
-    else {
-    	onDuty.push(vehicle['ranger' + (i + 1)]);
-    }
-  }
+console.log('s2: ' + s2.color);
+s2.color = 'blue';
 
-  for (var j = 0; j < vehicle.numRangers; j++) {
-    delete vehicle['ranger' + (j + 1)];
-  }
-  vehicle.numRangers = 0;
-
-  for (k = 0; k < onDuty.length; k++) {
-  	 vehicle['ranger' + (k + 1)] = onDuty[k];
-  }
-  vehicle.numRangers = onDuty.lenght;
-
-  return offDuty;
-}
-
-var offToday = relieveDuty(vehicle3, 'Friday');
+console.log('s2: ' + s2.color);
+console.log('shoe:' + shoe.color);
