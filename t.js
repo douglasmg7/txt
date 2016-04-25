39233d0d22cb095b1a61f5ccacbbdb1051f50476
Douglas Gomes <douglasmg7@gmail.com>
2016-04-25 19:15:30 -0300

    New files

--- t.js
+++ t.js
@@ -1,23 +1,6 @@
-// function ActiveUsersComponent(target, topicId){
-//   this.targetElement = target;
-//   this.topicId       = topicId;
-// }
+let text = `Hi,
+Este é um grande texto
 
-// ActiveUsersComponent.prototype.render = function(){
-//   getActiveUsers(this.topicId, (data) => {
-//     let userNameDivs = appendUserNames(...data.userNames);
-//     appendToSidebar(this.targetElement, userNameDivs);
-//   });
-// };
-// // Create new component below
-
-// let component = new ActiveUsersComponent('.active-users', 17);
-// component.render();
-CAR = {
-	part: 'Motor',
-	MOTOR: {
-		cil: '2000'
-	}
-}
-console.log(CAR.MOTOR.cil);
-console.log(MOTOR.cil);
+terceira linha 
+ fim.`;
+console.log(text);
