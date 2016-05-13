d1670b93e42d9ee174c022a8a9cc6d8ff96fba93
Douglas Gomes <douglasmg7@gmail.com>
2016-05-13 16:09:05 -0300

    new studies

--- t.js
+++ t.js
@@ -1,6 +1,6 @@
-let users = [
-	{login: 'Sam', admin: false},
-	{login: 'Brook', admin: true},
-	{login: 'Tyle', admin: true}];
-let admin = users.find(user => user.admin);
-console.log(admin);
+$(function() {
+	​var selections = []; 
+	$(".niners").click(function() { // this closure has access to the selections variable​
+		selections.push (this.prop("name")); // update the selections variable in the outer function's scope​
+	});
+});
