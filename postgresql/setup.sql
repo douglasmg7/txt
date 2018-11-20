d8060486a58c3ce361fa9de53bc0bfcd2a65ed02
douglasmg7 <douglasmg7@gmail.com>
2018-11-20 10:35:29 -0200

    postgres

---
+++ postgresql/setup.sql
@@ -0,0 +1,8 @@
+create table users (
+  id serial primary key,
+  uuid varchar(64) not null unique,
+  name varchar(255),
+  email varchar(255) not null unique,
+  password varchar(255) not null,
+  created_at timestamp not null
+);
