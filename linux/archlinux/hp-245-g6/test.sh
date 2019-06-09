66b06f1505ccb70e5fa6d3f8a38d7c163d771371
douglasmg7 <douglasmg7@gmail.com>
2019-06-09 14:52:32 -0300

    test 4-install

---
+++ linux/archlinux/hp-245-g6/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+printf "Creating Xserver config file..." 
+car > ~/.xserverrc << EOF
+#!/bin/sh
+exec /usr/bin/Xorg -nolisten tcp "$@" vt$XDG_VTNR
+EOF
+
+printf "Creating symbolic link for .xinitrc..."
+$ ln -s ~/dotfiles/xinitrc ~/.xinitrc
+
+# .Xresources
+printf "Creating symbolic link for .Xresources..."
+$ ln -s ~/dotfiles/Xresources ~/.Xresources
