39db0691f9ef66fef203dc9dda4f99806200e5f1
douglasmg7 <douglasmg7@gmail.com>
2019-06-18 13:25:58 -0300

    vim GoInstallBinaries

--- linux/archlinux/hp-245-g6/3-install.sh
+++ linux/archlinux/hp-245-g6/3-install.sh
@@ -117,6 +117,7 @@ mkdir -p ~/.vim && mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 vim +PluginInstall +q +q
+vin +GoInstallBinaries +q
 
 printf "\nInstalling neovim..."
 # sudo pacman -S neovim --noconfirm
