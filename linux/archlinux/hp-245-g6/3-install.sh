145d82118683a221b3dd6cf5794e9cc0eb9e3a1a
douglasmg7 <douglasmg7@gmail.com>
2019-06-18 20:12:19 -0300

    youcompleteme plugin install

--- linux/archlinux/hp-245-g6/3-install.sh
+++ linux/archlinux/hp-245-g6/3-install.sh
@@ -97,7 +97,7 @@ printf "\nEnabling ssh server..."
 sudo systemctl enable sshd
 sudo systemctl start sshd
 
-printf "\nCreating symbolic link to ssh configuration file..."
+printf "\nCreating symbolic link to ssh configuration file...\n"
 mkdir -p ~/.ssh
 ln -s ~/dotfiles/ssh/config ~/.ssh/config
 cp ~/dotfiles/ssh/ssh-agent.service ~/.ssh/ssh-agent.service
@@ -110,18 +110,25 @@ printf "\nStarting/Enabling bluetooth service...\n"
 sudo systemctl start bluetooth.service
 sudo systemctl enable bluetooth.service
 
-printf "\nInstalling vim..."
-# sudo pacman -S vim --noconfirm
-printf "\n\nSetting vim configuration..."
+printf "\nInsalling cmake...\n"   # Needed by YouCompleteMe vim plugin.
+sudo pacman -S cmake --noconfirm
+
+printf "\nInstalling vim...\n"
+sudo pacman -S vim --noconfirm
+printf "\nSetting vim configuration...\n"
 mkdir -p ~/.vim && mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 vim +PluginInstall +q +q
-vin +GoInstallBinaries +q
+vim +GoInstallBinaries +q   # Needed by vim-go plugin.
+# Compile YouCompleteMe components.
+printf "\nCompiling YCM components...\n"
+cd ~/.vim/bundle/youcompleteme
+python3 install.py --go-completer --ts-completer
 
-printf "\nInstalling neovim..."
-# sudo pacman -S neovim --noconfirm
-printf "\n\nSetting neovim configuration..."
+printf "\nInstalling neovim...\n"
+sudo pacman -S neovim --noconfirm
+printf "\nSetting neovim configuration...\n"
 mkdir -p ~/.config/nvim
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 sudo pacman -S python-neovim --noconfirm
