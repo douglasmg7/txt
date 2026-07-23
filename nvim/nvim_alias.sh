6c3b82177fb0a5d0f6cf1284911166e092d85c03
douglasmg7 <douglasmg7@gmail.com>
2026-07-23 14:27:31 -0300

    nvim plgins

---
+++ nvim/nvim_alias.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## default .config: ~/.config/nvim
+## multiple .configs: ~/.config/nvim-_
+
+alias nvim-lazy='NVIM_APPNAME="nvim-lazyvim" nvim'
+# rm -rf ~/.config/nvim-lazyvim ~/.local/share/nvim-lazyvim ~/.cache/nvim-lazyvim ~/.local/state/nvim-lazyvim
+
+alias nvim-nvchad='NVIM_APPNAME="nvim-nvchad" nvim'
+# rm -rf ~/.config/nvim-nvchad ~/.local/share/nvim-nvchad ~/.cache/nvim-nvchad ~/.local/state/nvim-nvchad
+
+alias nvim-astro='NVIM_APPNAME="nvim-astronvim" nvim'
+# rm -rf ~/.config/nvim-astronvim ~/.local/share/nvim-astronvim ~/.cache/nvim-astronvim ~/.local/state/nvim-astronvim
+
+alias nvim-kickstart='NVIM_APPNAME="nvim-kickstart" nvim'
+# rm -rf ~/.config/nvim-kickstart ~/.local/share/nvim-kickstart ~/.cache/nvim-kickstart ~/.local/state/nvim-kickstart
